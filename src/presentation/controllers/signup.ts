ae6953a46dd9cffd257cfe1052322200fbe88a28
Ivo Fernandes <ivofernandes@kk.pt>
2020-06-16 12:32:57 +0100

    refactor: move all errir imports to a single file

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,8 @@
 import { HttpResponse, HttpRequest } from '../protocolls/http'
-import { MissingParamError } from '../errors/missing-param-error'
+import { MissingParamError, InvalidParamError } from '../errors'
 import { badRequest, serverError } from '../helpers/http-helper'
 import { Controller } from '../protocolls/controller'
 import { EmailValidator } from '../protocolls/email-validator'
-import { InvalidParamError } from '../errors/invalid-param-error'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
