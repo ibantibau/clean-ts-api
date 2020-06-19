3e34c4bba1acf74ddf7a1ff7620ab306251cbebf
Ivo Fernandes <ivofernandes@kk.pt>
2020-06-19 14:41:58 +0100

    feat: coonect to mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,8 @@
-import app from './config/app'
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+import env from './config/env'
 
-app.listen(50500, () => console.log('Server runnig at http://localhost:50500'))
+MongoHelper.connet(env.mongoUrl).then( async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () => console.log(`Server runnig at http://localhost:${env.port}`))
+})
+.catch(console.error)
