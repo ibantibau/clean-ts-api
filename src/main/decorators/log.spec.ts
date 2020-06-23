1d27e900d9e55679e8c507b8bb41887858db1346
Ivo Fernandes <ivofernandes@kk.pt>
2020-06-23 12:18:34 +0100

    refactor: move sut creation to a factory method

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,24 +1,40 @@
 import { LogControllerDecorator } from "./log";
 import { Controller, HttpRequest, HttpResponse } from "../../presentation/protocols";
-import { resolve } from "path";
 
-describe('LogController Decorator', () => {
+const makeController = (): Controller => {
   class ControllerStub implements Controller {
     async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
       const httpResponse: HttpResponse = {
         statusCode: 200,
         body: {
-          name: 'Ivo Fernandes'
+        name: 'Ivo Fernandes'
         }
       }
       return new Promise(resolve => resolve(httpResponse))
     }
   }
+  return new ControllerStub()
+}
+
+interface SutTypes {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController()
+  const sut = new LogControllerDecorator(controllerStub)
+  return {
+    sut,
+    controllerStub
+  }
+}
+
+describe('LogController Decorator', () => {
 
   test('Should call controller handle', async () => {
-    const controllerStub = new ControllerStub()
+    const { sut, controllerStub } = makeSut()
     const handleSpy = jest.spyOn(controllerStub, 'handle')
-    const sut = new LogControllerDecorator(controllerStub)
     const httpRequest = {
       body: {
         name: 'any_name',
