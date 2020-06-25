41207aeb1f1011bddfbf3f4e4e5fa0111d6c4f50
Ivo Fernandes <ivofernandes@kk.pt>
2020-06-25 11:25:53 +0100

    feat: ensure LogMongoRepository creates an error log on success

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,28 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { Collection } from "mongodb"
+import { LogMongoRepository } from './log'
+
+
+describe('Log Mongo Repository', () => {
+  let errorCollection: Collection
+
+  beforeAll(async () => {
+    await MongoHelper.connet(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    errorCollection = await MongoHelper.getCollection('errors')
+    await errorCollection.deleteMany({})
+  })
+
+  test('Should create an log error on success', async () => {
+      const sut = new LogMongoRepository
+      await sut.logError('any_error')
+      const count = await errorCollection.countDocuments()
+      expect(count).toBe(1)
+  })
+})
